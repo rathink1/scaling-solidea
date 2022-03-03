ea51272bc1607e99bc7818b5923616c70baf7bf2
Rathin Kumar <rathin.kumar@bazaarvoice.com>
2022-03-03 14:44:36 +0530

    first commit

---
+++ programs/solidea/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FvfiKfUPLZvrAJnmeAZtMZi51FvYuoWr9WvozxDrb6aE");
+
+#[program]
+pub mod solidea {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
